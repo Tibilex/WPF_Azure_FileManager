c7f7a4d1e0560dffc13be83ef9a0d58a97b199dc
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-02-12 14:55:00 +0200

    Add: drag window Add: custom buttons "close/minimize" Add: fonts Add: styles

--- Azure_Blobs_FileManager/Views/MainWindow.xaml.cs
+++ Azure_Blobs_FileManager/Views/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Azure_Blobs_FileManager
 {
@@ -20,9 +9,29 @@ namespace Azure_Blobs_FileManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static MainWindow? _window;
         public MainWindow()
         {
             InitializeComponent();
+            _window = this;
+        }
+
+        private void DragWindow(object sender, RoutedEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                _window?.DragMove();
+            }
+        }
+
+        private void CloseWindow(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeWindow(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
